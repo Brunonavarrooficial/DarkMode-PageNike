b1e5e9994cc1d94c52fc1626452c661d66d368e6
Brunonavarrooficial <bnacessoria@gmail.com>
2022-10-22 22:13:18 -0300

    CommandScript-FinishedCSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
 const changeThemeBtn = document.querySelector('#change-theme');
 
-changeThemeBtn.addEventListener('change', function() {
+// toggle dark mode
+
+function toggleDarkMode() {
     document.body.classList.toggle('dark');
+}
+
+// Load ligth or dark mode in Local Storage
+function LoadTheme() {
+    const darkMode = localStorage.getItem('dark');
+
+    if (darkMode) {
+        toggleDarkMode();
+    }
+}
+
+LoadTheme();
+
+// change theme and save local storage
+changeThemeBtn.addEventListener('change', function () {
+    toggleDarkMode()
+
+    // Save or remove dark mode
+    localStorage.removeItem('dark');
+
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('dark', 1);
+    }
 });
